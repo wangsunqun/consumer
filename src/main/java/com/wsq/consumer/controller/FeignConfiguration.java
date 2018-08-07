4393fbc1931d45b70f491ebd6fa24441db0204fe
victor_qian <victor.qian@nakedhub.com>
2018-08-07 15:49:10 +0800

    修改调用baseService

--- src/main/java/com/wsq/consumer/controller/FeignConfiguration.java
+++ src/main/java/com/wsq/consumer/controller/FeignConfiguration.java
@@ -1,6 +1,7 @@
 package com.wsq.consumer.controller;
 
-import com.wsq.providerapi.feignInterface.fallback.UserHystrix;
+import com.wsq.baseapi.feignInterface.fallback.UserHystrix;
+//import com.wsq.providerapi.feignInterface.fallback.UserHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/com/wsq/consumer/controller/UserController.java
+++ src/main/java/com/wsq/consumer/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.wsq.consumer.controller;
 
-import com.wsq.providerapi.dto.UserDto;
-import com.wsq.providerapi.feignInterface.UserInterface;
+import com.wsq.baseapi.dto.UserDto;
+import com.wsq.baseapi.feignInterface.UserInterface;
+//import com.wsq.providerapi.dto.UserDtoto;
+//import com.wsq.providerapi.feignInterface.UserInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
